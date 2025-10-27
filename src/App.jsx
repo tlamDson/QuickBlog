69a52813ea7846bdcf404c19cf57f64d3e62a766
Lam Pham <phamla@dickinson.edu>
2025-10-27 00:35:24 -0400

    Initialize basic components of the admin dashboard and set up the route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddBlog from "./pages/admin/AddBlog";
+import ListBlog from "./pages/admin/ListBlog";
+import Comments from "./pages/admin/Comments";
+import Login from "./components/admin/Login";
 
 const App = () => {
   return (
@@ -9,6 +15,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:_id" element={<Blog />} />
+        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+          <Route index elemen={<Dashboard />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="listBlog" element={<ListBlog />} />
+          <Route path="comments" element={<Comments />} />
+        </Route>
       </Routes>
     </div>
   );
