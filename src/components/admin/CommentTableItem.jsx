3e0ea2e290cb0f55faa4e5ee73698e59a51c286c
Lam Pham <phamla@dickinson.edu>
2025-11-18 23:09:34 -0500

    Implement comment approval and deletion functionality in CommentTableItem component, enhance Comments page to fetch comments from API with improved error handling, and integrate toast notifications for user feedback.

--- src/components/admin/CommentTableItem.jsx
+++ src/components/admin/CommentTableItem.jsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
 import { assets } from "../../assets/assets";
+import { useAppContext } from "../../../context/AppContext";
+import toast from "react-hot-toast";
 
 const CommentTableItem = ({ comment, fetchComments }) => {
   const { blog, createAt, _id } = comment;
   const BlogDate = new Date(createAt);
+  const {axios} = useAppContext();
+
+  const approveComment = async () => {
+    try {
+      const {data} = await axios.post('/api/admin/approve-comment',{id : _id})
+      if (data.success) { 
+        toast.success(data.message)
+        fetchComments();
+      }
+      else { 
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  };
+  const deleteComment = async () => {
+    try {
+      const confirm = window.confirm('Are you sure you want to delete this comment')
+      if (!confirm) return;
+      const {data} = await axios.post('/api/admin/delete-comment',{id : _id})
+      if (data.success) { 
+        toast.success(data.message)
+        fetchComments();
+      }
+      else { 
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+  };
   return (
     <tr className="border-y border-gray-300">
       <td className="px-6 py-4">
@@ -21,6 +55,7 @@ const CommentTableItem = ({ comment, fetchComments }) => {
         <div className="inline-flex items-center gap-4">
           {!comment.isApproved ? (
             <img
+            onClick={approveComment}
               src={assets.tick_icon}
               className="w-5 hover:scale-110 transition-all cursor-pointer"
             />
@@ -30,6 +65,7 @@ const CommentTableItem = ({ comment, fetchComments }) => {
             </p>
           )}
           <img
+          onClick={deleteComment}
             src={assets.bin_icon}
             alt=""
             className="w-5 hover:scale-110 transition-all cursor-pointer"
--- src/pages/admin/Comments.jsx
+++ src/pages/admin/Comments.jsx
@@ -1,13 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { comments_data } from "../../assets/assets";
 import CommentTableItem from "../../components/admin/CommentTableItem";
+import { useAppContext } from "../../../context/AppContext";
 
 const Comments = () => {
   const [comments, setComments] = useState([]);
   const [filter, setFilter] = useState("Not Approved");
-
+  const {axios} = useAppContext();
+  
   const fetchComments = async () => {
-    setComments(comments_data);
+    try {
+      const {data} = await axios.get('/api/admin/comments')
+      data.success ? setComments(data.comments) : toast.error(data.error)
+    } catch (error) {
+      toast.error(error.message)
+    }
   };
   useEffect(() => {
     fetchComments();
