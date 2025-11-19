29ee1c28488368522a369e36ca49bd7d84eb4e9f
Lam Pham <phamla@dickinson.edu>
2025-11-18 21:15:44 -0500

    In App.jsx replace true with the checking in the token. Fixing not using object value in value so that the navigate is undefined.

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -1,45 +1,55 @@
-import {createContext, useContext, useEffect, useState} from "react";
-import axios from 'axios'
-import {useNavigate} from 'react-router-dom'
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 //instead of having the navigate hook, we can create it here
 const AppContext = createContext();
 
-export const AppProvider = ({children}) => { 
-    const navigate = useNavigate();
-    //store the token for authentication
-    const [token,setToken] = useState(null);
-    const [blogs,setBlogs] = useState([]);
-    const [input,setInput] = useState("");
+export const AppProvider = ({ children }) => {
+  const navigate = useNavigate();
+  //store the token for authentication
+  const [token, setToken] = useState(null);
+  const [blogs, setBlogs] = useState([]);
+  const [input, setInput] = useState("");
 
-
-    const fetchBlogs = async () => { 
-        try {
-            const {data} = await axios.get('/blog/all')
-            data.succuess ? setBlogs(data.blogs) : toast.error(data.message)
-        } catch (error) {
-             toast.error(error.message)
-        }
+  const fetchBlogs = async () => {
+    try {
+      const { data } = await axios.get("/blog/all");
+      data.succuess ? setBlogs(data.blogs) : toast.error(data.message);
+    } catch (error) {
+      toast.error(error.message);
     }
-    useEffect(() => { 
-        fetchBlogs();
-    },[])
-    //we can access this data in another component
-    const value = {
-        axios,navigate,token,setToken,blogs,setBlogs,input,setInput
+  };
+  useEffect(() => {
+    fetchBlogs();
+    const token = localStorage.getItem("token");
+    if (token) {
+      setToken(token);
+      //this token will be added in every api call when admin is loged in
+      axios.defaults.headers.common["Authorization"] = `${token}`;
     }
-    return ( 
-        
-        <AppContext.Provider value={{value}}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+  }, []);
+  //we can access this data in another component
+  const value = {
+    axios,
+    navigate,
+    token,
+    setToken,
+    blogs,
+    setBlogs,
+    input,
+    setInput,
+  };
+  return (
+    //wrong in wrapping value inside another object s owhen call the naviagate is undefined
+    <AppContext.Provider value={value}>{children}</AppContext.Provider>
+  );
+};
 //whenever we want to use the data from the context we will use this hook
-export const useAppContext = () => { 
-    return useContext(AppContext);
-}
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
 
 //we will create some data that will be share in the components
--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,17 @@ import Comments from "./pages/admin/Comments";
 import Login from "./components/admin/Login";
 //import the css of quill for react
 import "quill/dist/quill.snow.css";
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from "react-hot-toast";
+import { useAppContext } from "../context/AppContext";
 const App = () => {
+  const { token } = useAppContext();
   return (
     <div>
       <Toaster />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:_id" element={<Blog />} />
-        <Route path="/admin" element={true ? <Layout /> : <Login />}>
+        <Route path="/admin" element={token ? <Layout /> : <Login />}>
           <Route index element={<Dashboard />} />
           <Route path="addBlog" element={<AddBlog />} />
           <Route path="listBlog" element={<ListBlog />} />
